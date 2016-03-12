299c96994830abd9bbea3a90b6f62dace6929964
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-12 11:46:16 -0500

    Create Client

---
+++ src/Creacionales/AbstractFactory/Empresa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Creacionales.AbstractFactory;
+
+/**
+ * Empresa
+ *
+ * @author Yeicol
+ */
+public class Empresa {
+
+    private LCD lcd;
+    private Plasma plasma;
+
+    public Empresa(ITelevisorFactory fabrica) {
+        this.lcd = fabrica.ConstruirLCD();
+        this.plasma = fabrica.ConstruirPlasma();
+    }
+
+    public void formatoLCD() {
+        this.lcd.producir();
+    }
+
+    public void formatoPlasma() {
+        this.plasma.producir();
+    }
+}
