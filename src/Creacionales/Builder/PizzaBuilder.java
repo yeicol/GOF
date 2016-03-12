19fe91083ae8411fb08cb0fc010d9228a2555ed0
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-12 14:43:11 -0500

    Create Builder

---
+++ src/Creacionales/Builder/PizzaBuilder.java
@@ -0,0 +1,15 @@
+package Creacionales.Builder;
+
+/**
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ * @author Madelin Rangel Grisales
+ */
+public interface PizzaBuilder {
+
+    public void agregarMasa();
+
+    public void agregarSalsa();
+
+    public void agregarRelleno();
+}
