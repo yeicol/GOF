4f9ec06cc802a720bd92e58d1ea8f15ce977fa0e
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-15 07:57:28 -0500

    Create Client

---
+++ src/Estructurales/Proxy/EditorDocumentos.java
@@ -0,0 +1,13 @@
+package Estructurales.Proxy;
+
+/**
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ * @author Madelin Rangel Grisales
+ */
+public class EditorDocumentos {
+
+    public void cargarDocumento(Grafico grafico) {
+        grafico.dibujar();
+    }
+}
