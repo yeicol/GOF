f763a45a495a761c6366a3c40d29576b058d94bb
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-16 18:47:29 -0500

    Create decorator

---
+++ src/Estructurales/Decorator/DecoradorCombo.java
@@ -0,0 +1,11 @@
+package Estructurales.Decorator;
+
+/**
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ * @author Madelin Rangel Grisales
+ */
+public abstract class DecoradorCombo extends Combo {
+
+	public abstract String getDescripcion();
+}
