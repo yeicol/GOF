d9076db602ca9cf1dfc520dc79df77b68af9a1ff
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-16 20:03:41 -0500

    Add Creator

---
+++ src/Creacionales/FactoryMethod/TrianguloFabrica.java
@@ -0,0 +1,10 @@
+package Creacionales.FactoryMethod;
+
+/**
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ * @author Madelin Rangel Grisales
+ */
+public interface TrianguloFabrica {
+	public Triangulo crearTringulo(int ladoA, int ladoB, int ladoC);
+}
