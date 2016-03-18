9fe67c990ce168a0b4483c8e56a65cb2c6ad8f82
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-03-18 13:50:24 -0500

    Crear Facade

--- src/Estructurales/Facade/Inmobiliaria.java
+++ src/Estructurales/Facade/Inmobiliaria.java
@@ -7,8 +7,33 @@ package Estructurales.Facade;
 
 /**
  *
- * @author Administrador
+ * @author Brian Yeicol Restrepo Tangarife
+ * @author Madelin Rangel Grisales
  */
 public class Inmobiliaria {
-    
+
+	private Alquiler alquiler;
+	private Inmueble inmueble;
+	private Cliente cliente;
+	private Vendedor vendedor;
+
+	public Inmobiliaria(Alquiler alquiler, Inmueble inmueble, Cliente cliente, Vendedor vendedor) {
+		this.alquiler = alquiler;
+		this.inmueble = inmueble;
+		this.cliente = cliente;
+		this.vendedor = vendedor;
+	}
+
+	public void cobrarAlquiler() {
+		alquiler.cobrar();
+		cliente.efectuarPago();
+		vendedor.registrarPago();
+	}
+
+	public void alquilarInmueble() {
+		inmueble.mostrar();
+		vendedor.exhibirInmueble();
+		vendedor.elaborarContrato();
+		cliente.firmarContrato();
+	}
 }
